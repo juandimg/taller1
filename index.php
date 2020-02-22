5802fe07e5e98ab31b6d693ccbd3bc7f08331ba9
juandimg <61071152+juandimg@users.noreply.github.com>
2020-02-21 21:14:06 -0500

    operaciones completas

--- index.php
+++ index.php
@@ -11,7 +11,17 @@ $resta=$numero1-$numero2;
 $multiplicacion=$numero1*$numero2;
 $division=$numero1/$numero2;
 
-echo("<br>La suma de: ".$numero1."+".$numero2."=".$suma)
+echo("<br>La suma de: ".$numero1."+".$numero2."=".$suma);
+
+echo("<br>La resta de: ".$numero1."-".$numero2."=".$resta);
+
+echo("<br>La multiplicacion de: ".$numero1."*".$numero2."=".$multiplicacion);
+
+echo("<br>La division de: ".$numero1."/".$numero2."=".$division );
+
+
+echo("<br><br>punto 2");
+
 
 
 
