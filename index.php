7f76572d8a76ef3311db068fbb4ab44c7f1c1eba
juandimg <61071152+juandimg@users.noreply.github.com>
2020-02-21 21:04:40 -0500

    resultado suma

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
 
+echo("Taller elaborado por Juan Diego Montes <br>");
+echo("punto 1");
 
-echo("Hola mundo");
+$numero1=10;
+$numero2=25;
+
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$division=$numero1/$numero2;
+
+echo("<br>La suma de: ".$numero1."+".$numero2."=".$suma)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
